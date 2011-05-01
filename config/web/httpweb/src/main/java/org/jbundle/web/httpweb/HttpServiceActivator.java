6c7d0b4f4f854aa4c629c6ab9599dd5cd9bad6cf
doncorley <don@tourgeek.com>
2011-05-01 00:09:26 -0700

    Split httpservice and web files

---
+++ config/web/httpweb/src/main/java/org/jbundle/web/httpweb/HttpServiceActivator.java
@@ -0,0 +1,10 @@
+package org.jbundle.web.httpweb;
+
+
+/**
+ * Start up the web service listener.
+ * @author don
+ */
+public class HttpServiceActivator extends org.jbundle.web.httpservice.HttpServiceActivator
+{
+}
