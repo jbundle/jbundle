bd4c4a78e59b3ff8efd9d25196d9ebb0b21c2912
doncorley <don@tourgeek.com>
2011-06-01 02:34:09 -0700

    Add thin screen activator

---
+++ thin/base/mixed/src/main/java/org/jbundle/thin/base/screen/ThinActivator.java
@@ -0,0 +1,83 @@
+/**
+ * RmiSessionServer.java
+ *
+ * Created on January 10, 2000, 4:47 PM
+ */
+ 
+package org.jbundle.thin.base.screen;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+import org.jbundle.model.BaseAppletReference;
+import org.jbundle.model.Task;
+import org.jbundle.model.util.Util;
+import org.jbundle.thin.base.util.Application;
+import org.jbundle.util.osgi.bundle.BaseBundleService;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceEvent;
+
+public class ThinActivator extends BaseBundleService
+{
+	protected BaseAppletReference applet = null;
+	
+    String[] args = {
+    		"menu=Thin",
+//x    		"background=backgrounds/worldmapalpha.gif",
+//x    		"backgroundcolor=#EEEEFF",
+//x    		"mainSharedDBName=main_base",
+//x    		"local=Jdbc",
+//x    		"remote=Jdbc",
+//x    		"table=Jdbc",
+    		"connectionType=proxy",
+    		"codebase=localhost:8181/",
+    		"server=localhost",
+    		};	// TODO(don) Fix this
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+	}
+
+    @Override
+    public void serviceChanged(ServiceEvent event) {
+        if (event.getType() == ServiceEvent.REGISTERED)
+        { // Osgi Service is up, Okay to start the server
+            System.out.println("Starting Applet");
+    		if (applet == null)
+    		{
+    	        Map<String,Object> propertiesTemp = new Hashtable<String,Object>();
+    	        Util.parseArgs(propertiesTemp, args);
+
+    	        //?server = new SApplet(args);
+    	        ThinApplet.main(args);
+    	        applet = Application.getRootApplet();
+
+//?    	        Environment env = new Environment(propertiesTemp);
+    	        // Note the order that I do this... this is because MainApplication may need access to the remoteapp during initialization
+    		}
+        }
+        if (event.getType() == ServiceEvent.UNREGISTERING)
+        {
+            if (applet != null)
+            {
+            	if (((Task)(applet)).getApplication() != null)
+            		((Task)(applet)).getApplication().free();
+            	else
+            		applet.free();
+            }
+            applet = null;
+        }        
+    }
+}
