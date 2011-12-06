c5144bb3cf9370e41b9951f7c643a120177b57cf
Don Corley <don@donandann.com>
2011-12-06 09:50:46 -0800

    Remove rmi dependencies

--- app/test/src/main/java/org/jbundle/app/test/vet/remote/CatSession.java
+++ app/test/src/main/java/org/jbundle/app/test/vet/remote/CatSession.java
@@ -5,13 +5,25 @@
  */
 package org.jbundle.app.test.vet.remote;
 
-import org.jbundle.app.test.vet.db.Cat;
-import org.jbundle.app.test.vet.db.Vet;
-import org.jbundle.base.db.Record;
-import org.jbundle.base.db.filter.SubFileFilter;
-import org.jbundle.base.remote.BaseSession;
-import org.jbundle.base.remote.db.Session;
-import org.jbundle.thin.base.remote.RemoteException;
+import java.awt.*;
+import java.util.*;
+
+import org.jbundle.base.db.*;
+import org.jbundle.thin.base.util.*;
+import org.jbundle.thin.base.db.*;
+import org.jbundle.base.db.event.*;
+import org.jbundle.base.db.filter.*;
+import org.jbundle.base.field.*;
+import org.jbundle.base.field.convert.*;
+import org.jbundle.base.field.event.*;
+import org.jbundle.base.screen.model.*;
+import org.jbundle.base.screen.model.util.*;
+import org.jbundle.base.util.*;
+import org.jbundle.model.*;
+import org.jbundle.base.remote.db.*;
+import org.jbundle.base.remote.*;
+import org.jbundle.thin.base.remote.*;
+import org.jbundle.app.test.vet.db.*;
 
 /**
  *  CatSession - .
