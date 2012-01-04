f6757488a5ecaaf3ec65729c01a7aeff69e0ca9d
Don Corley <don@donandann.com>
2012-01-04 10:44:58 -0800

    Fix get bundle from package name method

--- app/program/demo/src/main/java/org/jbundle/app/program/demo/BaseSetupSiteProcess.java
+++ app/program/demo/src/main/java/org/jbundle/app/program/demo/BaseSetupSiteProcess.java
@@ -5,48 +5,37 @@
  */
 package org.jbundle.app.program.demo;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.URL;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Map;
+import java.awt.*;
+import java.util.*;
 
-import org.jbundle.app.program.demo.message.CreateSiteMessageData;
-import org.jbundle.app.program.demo.message.MenusMessageData;
-import org.jbundle.app.program.manual.convert.ConvertCode;
-import org.jbundle.app.program.script.scan.BaseScanListener;
-import org.jbundle.app.program.script.scan.DeleteScanListener;
-import org.jbundle.app.program.script.scan.XMLScanListener;
-import org.jbundle.base.db.Record;
-import org.jbundle.base.field.DateField;
-import org.jbundle.base.field.PropertiesField;
-import org.jbundle.base.message.remote.BaseMessageProcess;
-import org.jbundle.base.message.remote.RunRemoteProcessMessageData;
-import org.jbundle.base.message.remote.StandardMessageResponseData;
-import org.jbundle.base.thread.BaseProcess;
-import org.jbundle.base.util.BaseApplication;
-import org.jbundle.base.util.DBConstants;
-import org.jbundle.base.util.DBParams;
-import org.jbundle.base.util.Utility;
-import org.jbundle.main.db.Menus;
-import org.jbundle.main.user.db.UserGroup;
-import org.jbundle.main.user.db.UserInfo;
-import org.jbundle.model.DBException;
-import org.jbundle.model.RecordOwnerParent;
-import org.jbundle.model.Task;
-import org.jbundle.thin.base.db.buff.BaseBuffer;
-import org.jbundle.thin.base.db.buff.VectorBuffer;
-import org.jbundle.thin.base.message.BaseMessage;
-import org.jbundle.thin.base.message.MessageRecordDesc;
-import org.jbundle.thin.base.message.TreeMessage;
-import org.jbundle.thin.base.thread.AutoTask;
-import org.jbundle.util.osgi.finder.ClassServiceUtility;
-import org.jbundle.util.osgi.webstart.OsgiJnlpServlet;
-import org.osgi.framework.Bundle;
+import org.jbundle.base.db.*;
+import org.jbundle.thin.base.util.*;
+import org.jbundle.thin.base.db.*;
+import org.jbundle.base.db.event.*;
+import org.jbundle.base.db.filter.*;
+import org.jbundle.base.field.*;
+import org.jbundle.base.field.convert.*;
+import org.jbundle.base.field.event.*;
+import org.jbundle.base.screen.model.*;
+import org.jbundle.base.screen.model.util.*;
+import org.jbundle.base.util.*;
+import org.jbundle.model.*;
+import org.jbundle.base.message.remote.*;
+import org.jbundle.main.db.*;
+import org.jbundle.main.user.db.*;
+import org.jbundle.thin.base.db.buff.*;
+import org.jbundle.thin.base.thread.*;
+import java.net.*;
+import java.io.*;
+import org.jbundle.app.program.script.scan.*;
+import org.jbundle.app.program.manual.convert.*;
+import java.text.*;
+import org.jbundle.app.program.demo.message.*;
+import org.jbundle.thin.base.message.*;
+import org.jbundle.base.thread.*;
+import org.osgi.framework.*;
+import org.jbundle.util.osgi.webstart.*;
+import org.jbundle.util.osgi.finder.*;
 
 /**
  *  BaseSetupSiteProcess - .
@@ -321,6 +310,11 @@ public class BaseSetupSiteProcess extends BaseMessageProcess
             String packageName = templateDir + "/main_user/org/jbundle/main/user/db";
             packageName = packageName.replace('/', '.');
             Bundle bundle = (Bundle)ClassServiceUtility.getClassService().getClassFinder(null).findBundle(null, null, packageName, null);
+            if (bundle == null) {
+                Object resource = ClassServiceUtility.getClassService().getClassFinder(null).deployThisResource(packageName, null, true);
+                if (resource != null)
+                    bundle = (Bundle)ClassServiceUtility.getClassService().getClassFinder(null).findBundle(resource, null, packageName, null);
+            }
         
             if (bundle == null)
                 return false;   // Couldn't file files
