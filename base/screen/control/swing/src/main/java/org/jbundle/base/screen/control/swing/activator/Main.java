afbcffa412c0243195513b3df650b23cd20a8fb7
Don Corley <don@donandann.com>
2012-03-09 15:20:07 -0800

    Add java-exec for client to maven

---
+++ base/screen/control/swing/src/main/java/org/jbundle/base/screen/control/swing/activator/Main.java
@@ -0,0 +1,11 @@
+package org.jbundle.base.screen.control.swing.activator;
+
+public class Main extends org.jbundle.Main {
+
+    private static final long serialVersionUID = 1L;
+
+    public static void main(String args[])
+    {
+        org.jbundle.Main.main(args);
+    }
+}
