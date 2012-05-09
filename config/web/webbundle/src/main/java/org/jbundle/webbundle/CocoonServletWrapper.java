07d30b5b4317405a2ee56f758ae6e2e4ef9255ac
Don Corley <don@donandann.com>
2012-05-09 06:42:06 -0700

    Set up remote repositories

--- config/web/webbundle/src/main/java/org/jbundle/webbundle/CocoonServletWrapper.java
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright © 2012 jbundle.org. All rights reserved.
- */
-package org.jbundle.webbundle;
-
-public class CocoonServletWrapper /* extends org.jbundle.base.screen.control.servlet.xml.cocoon.CocoonServletWrapper */ {
-
-}
