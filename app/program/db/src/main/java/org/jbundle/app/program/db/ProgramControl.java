9d5b40e4a28fb9ff29429036f591753fb506419f
Don Corley <don@donandann.com>
2012-10-28 05:23:09 -0700

    Clean up build

--- app/program/db/src/main/java/org/jbundle/app/program/db/ProgramControl.java
+++ app/program/db/src/main/java/org/jbundle/app/program/db/ProgramControl.java
@@ -21,7 +21,6 @@ import org.jbundle.base.util.*;
 import org.jbundle.model.*;
 import org.jbundle.model.db.*;
 import org.jbundle.model.screen.*;
-import org.jbundle.base.screen.model.*;
 import org.jbundle.app.program.resource.db.*;
 import org.jbundle.model.app.program.db.*;
 
