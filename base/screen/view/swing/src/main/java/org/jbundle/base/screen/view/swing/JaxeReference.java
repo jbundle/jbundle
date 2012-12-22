e2a1506389754a1cde5ba94f55efc3cd1f0b46ef
Don Corley <don@donandann.com>
2012-12-21 16:44:40 -0800

    Move the screen view Swing implementation from the main jbundle project

--- base/screen/view/swing/src/main/java/org/jbundle/base/screen/view/swing/JaxeReference.java
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright © 2012 jbundle.org. All rights reserved.
- */
-package org.jbundle.base.screen.view.swing;
-
-public interface JaxeReference {
-    public static final String JAXE_EDITOR_CLASS = "org.jbundle.base.screen.view.swing.opt.jaxe.JaxeEditor";
-}
