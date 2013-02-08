d3aaefae957c372bdc1aaabd381cf0cb9776bd7f
Don Corley <don@donandann.com>
2013-02-07 17:13:33 -0800

    Fix the test data resource path

--- app/program/demo/src/main/java/org/jbundle/app/program/demo/BaseSetupSiteProcess.java
+++ app/program/demo/src/main/java/org/jbundle/app/program/demo/BaseSetupSiteProcess.java
@@ -309,7 +309,7 @@ public class BaseSetupSiteProcess extends BaseMessageProcess
         URL fromDirUrl = this.getTask().getApplication().getResourceURL(templateDir, null);
         if ("http".equalsIgnoreCase(fromDirUrl.getProtocol()))
         {
-            String packageName = templateDir + "/main_user/org/jbundle/main/user/db";
+            String packageName = templateDir + "/main_tourapp_user/org/jbundle/main/user/db";
             packageName = packageName.replace('/', '.');
             Bundle bundle = (Bundle)ClassServiceUtility.getClassService().getClassFinder(null).findBundle(null, null, packageName, null);
             if (bundle == null) {
